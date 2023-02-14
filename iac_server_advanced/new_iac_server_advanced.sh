fb61a37b13722e4c2719ef654b75ad7b5250ed2c
Jhones <jhonesaly@gmail.com>
2023-02-14 18:27:37 -0300

    Update project_iac2

--- iac_server_advanced/new_iac_server_advanced.sh
+++ iac_server_advanced/new_iac_server_advanced.sh
@@ -3,9 +3,15 @@
 ## 0 - Saudação
 
 printf "\nIniciando protocolo new_iac_server_advanced...\n"
-printf "\nEm caso de dúvida, consulte a documentação disponível em <>\n"
+printf "\nEm caso de dúvida, consulte a documentação disponível em <https://github.com/jhonesaly/project_iac2>\n"
 
 ## 1 - 
+
+## Cria servidor de arquivos
+
+## Cria servidor de web
+
+## Cria servidor de banco de dados
 
 ##  - Fim
 printf "\nFinalizado.\n"
--- iac_server_basic/new_iac_server_basic.sh
+++ iac_server_basic/new_iac_server_basic.sh
@@ -3,10 +3,26 @@
 ## 0 - Saudações
 
 printf "\nIniciando protocolo new_iac_server...\n"
-printf "\nEm caso de dúvida, consulte a documentação disponível em <>\n"
+printf "\nEm caso de dúvida, consulte a documentação disponível em <https://github.com/jhonesaly/project_iac2>\n"
 
-## 1 - 
+## 1 - Atualiza o servidor
 
+printf "\nAtualizando o servidor...\n"
+
+apt-get update
+apt-get upgrade -y
+apt-get install apache2 -y
+apt-get install unzip -y
+
+## 2 - Baixa arquivos do repositório e 
+
+printf "\nBaixando e copiando os arquivos da aplicação...\n"
+
+cd /tmp
+wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
+unzip main.zip
+cd linux-site-dio-main
+cp -R * /var/www/html/
 #  - Fim
 
 printf "\nFinalizado.\n"
