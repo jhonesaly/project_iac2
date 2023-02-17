6766d71380704dac70cf2b1c8208c629cabe80e0
Jhones <jhonesaly@gmail.com>
2023-02-17 17:40:38 -0300

    Update project_iac2

--- iac_server_advanced/new_iac_server_advanced.sh
+++ iac_server_advanced/new_iac_server_advanced.sh
@@ -17,6 +17,9 @@ while true; do
         if [ "$ans_a1" != "y" ] && [ "$ans_a1" != "n" ]; then
             printf "\nDigite um comando válido.\n"
             continue
+        else
+            question_number=2
+            continue
         fi
 
     elif [ $question_number -eq 2 ]; then
@@ -28,7 +31,9 @@ while true; do
 
         elif [ "$ans_a2" != "y" ] && [ "$ans_a2" != "n" ]; then
             printf "\nDigite um comando válido.\n"
-            question_number=2
+            continue
+        else
+            question_number=3
             continue
         fi
 
